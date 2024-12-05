b9db9b3b512cf79f82135c18f07b7eac3c14c553
holic-x <1301996554@qq.com>
2024-12-05 18:40:19 +0800

    feat：代理测试
    feat：代理测试

--- noob-rpc-base/src/main/java/com/noob/rpc/base/proxy/TestProxy.java
+++ noob-rpc-base/src/main/java/com/noob/rpc/base/proxy/TestProxy.java
@@ -1,21 +1,24 @@
 package com.noob.rpc.base.proxy;
 
 
-import org.springframework.cglib.proxy.Proxy;
+import java.lang.reflect.Proxy;
 
 /**
  * 代理测试
+ * todo https://www.jb51.net/article/252277.htm
  */
 public class TestProxy {
 
     public static void main(String[] args) {
         // 构建代理器
         JDKProxy jdkProxy = new JDKProxy(new RealHello());
-        ClassLoader classLoader = ClassLoaderUtils.getCurrentClassLoader();
+//        ClassLoader classLoader = ClassLoaderUtils.getCurrentClassLoader();
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+
         // 把生成的代理类保存到文件
-        System.setProperty("sun.misc.ProxyGenerator.saveGeneratedFiles","true");
+        System.setProperty("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
         // 生成代理类
-        Hello test = (Hello) Proxy.newProxyInstance(classLoader, new Class[]{Hello.class}, proxy);
+        Hello test = (Hello) Proxy.newProxyInstance(classLoader, new Class[]{Hello.class}, jdkProxy);
         // 方法调用
         System.out.println(test.say());
     }
