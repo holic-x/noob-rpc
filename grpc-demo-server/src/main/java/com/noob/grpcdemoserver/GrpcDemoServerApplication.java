de54880d7d6a97a29f9479501cc232a6d6144eaf
holic-x <1301996554@qq.com>
2024-12-18 16:14:32 +0800

    feat：grpc-demo
    feat：grpc-demo

---
+++ grpc-demo-server/src/main/java/com/noob/grpcdemoserver/GrpcDemoServerApplication.java
@@ -0,0 +1,13 @@
+package com.noob.grpcdemoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcDemoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcDemoServerApplication.class, args);
+    }
+
+}
